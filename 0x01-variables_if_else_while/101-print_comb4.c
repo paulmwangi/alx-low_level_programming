e6cdcb9e1e9de323ce1c3372f40c4513fd9a6642
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 14:30:02 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-/**
- * main - possible combos of 3 digits
- *
- * Return (0)
- */
-
-int main(void)
-{
-	int i, j, k;
-
-	for (i = 48; i < 58; i++)
-	{
-		for (j = 48; j < 58; j++)
-		{
-			for (k = 48; k < 58; k++)
-			{
-				if (i == j || j == k || i == k)
-				{
-					continue;
-				}
-				putchar(i);
-				putchar(j);
-				putchar(k);
-
-				if (i == 55 && j == 56 && k == 57)
-				{
-					break;
-
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-		putchar('\n');
-		return (0);
-	}
-}
-=======
 #include <stdio.h>
 
 /**
@@ -85,4 +41,3 @@ int main(void)
 	
 	return (0);
 }
->>>>>>> 63a54755c52068a189dc1f405020916338e2fb41
