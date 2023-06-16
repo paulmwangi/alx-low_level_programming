d234cbaa425f742c80d6c539e82cf12122b0955c
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 15:41:45 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - main - two pairs of numbers that do not repeat
+ * main - Generates pairs of numbers that do not repeat.
  *
- * Return (0)
+ * Return: 0 on successful execution
  */
-
 int main(void)
 {
-	int i, j;
+    int i, j;
 
-	for (j = 0; j <= 99; j++)
-	{
-		for (i = j; i <= 99; i++)
-		{
-			if (i != j)
-			{
-				putchar(j / 10 + 48);
-				putchar(j % 10 + 48);
-				putchar(' ');
-				putchar(i / 10 + 48);
-				putchar(i % 10 + 48);
-			}
-			if (j * 100 + i != 9899)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
+    for (j = 0; j <= 99; j++)
+    {
+        for (i = j; i <= 99; i++)
+        {
+            if (i != j)
+            {
+                putchar(j / 10 + '0');
+                putchar(j % 10 + '0');
+                putchar(' ');
+                putchar(i / 10 + '0');
+                putchar(i % 10 + '0');
+            }
+            if (j * 100 + i != 9899)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return 0;
 }
