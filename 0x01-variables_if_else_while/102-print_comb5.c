b6a68d8d6911a44b83d8720524dfe73c431cd968
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 15:47:16 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,28 +6,26 @@
  */
 int main(void)
 {
-    int i, j;
-
-    for (j = 0; j <= 99; j++)
-    {
-        for (i = j; i <= 99; i++)
-        {
-            if (i != j)
-            {
-                putchar(j / 10 + '0');
-                putchar(j % 10 + '0');
-                putchar(' ');
-                putchar(i / 10 + '0');
-                putchar(i % 10 + '0');
-            }
-            if (j * 100 + i != 9899)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return 0;
+int i, j;
+for (j = 0; j <= 99; j++)
+{
+for (i = j; i <= 99; i++)
+{
+if (i != j)
+{
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+putchar(' ');
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+}
+if (j * 100 + i != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return 0;
 }
