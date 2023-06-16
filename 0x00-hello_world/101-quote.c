59bfc9d4b9df3e0ea3fe5ac64b24523ad27c6e18
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 10:27:21 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
     char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
     write(STDERR_FILENO, message, strlen(message));
     return (1);
 }
