a7ba15c4fb8d667a08f8a60b2fa7e6c869ecda5e
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 16:29:43 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * This program prints the message "Programming is like building a multilingual puzzle"
- * using the puts function. It demonstrates how programming is similar to solving a puzzle,
- * requiring the assembly of different languages and components to create a complete solution.
- *
+ * main - Entry point of the program 
  * Return: Always 0 (indicating success)
  */
 int main(void)
