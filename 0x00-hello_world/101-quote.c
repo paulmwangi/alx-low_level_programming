4f09570630609971993a27e03ddd5b1652d73181
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 10:32:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(STDERR_FILENO, message, strlen(message));
-    return (1);
+write(STDERR_FILENO, message, strlen(message));
+return (1);
 }
