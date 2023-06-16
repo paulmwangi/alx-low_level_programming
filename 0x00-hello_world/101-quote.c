2ca2850b9472947c1cea083282318ff26e11e3b9
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 16:40:23 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,31 @@
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Return: Always 1 (Success)
+ * This program prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
+ * followed by a new line, to the standard error using the write function. It demonstrates how
+ * to write output directly to the standard error without using printf or puts functions.
+ *
+ * Return: Always 1 (indicating failure)
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, strlen(message));
-return (1);
+    int fd;
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = strlen(message);
+
+    fd = open("/dev/stderr", O_WRONLY);
+    if (fd == -1)
+        return 1;
+
+    if (write(fd, message, len) != len)
+        return 1;
+
+    close(fd);
+    return 1;
 }
