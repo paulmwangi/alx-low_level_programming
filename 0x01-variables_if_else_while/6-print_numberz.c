0746ea016a5d34bbd012a6dc1ef562f299d50875
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 13:32:05 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int main(void)
-{
-	int n = 48;
-
-	while (n < 58)
-	{
-		putchar(n);
-		n++;
-	}
-	putchar ('\n');
-
-	return (0);
-}
-=======
 #include <stdio.h>
 
 /**
@@ -36,4 +19,3 @@ int main(void)
 
 	return (0);
 }
->>>>>>> d2e616a51e64e42246c1b2aba5f012c87b65a144
