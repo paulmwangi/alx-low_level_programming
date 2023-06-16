bb19c31316f508b8dc98bf73c8c283d0a9612eac
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 14:39:04 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, generates all possible combinations of 3 digits
+ *        without repeating any digit.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+    int i, j, k;
+
+    for (i = 48; i < 58; i++)
+    {
+        for (j = 48; j < 58; j++)
+        {
+            for (k = 48; k < 58; k++)
+            {
+                if (i == j || j == k || i == k)
+                {
+                    continue;
+                }
+                putchar(i);
+                putchar(j);
+                putchar(k);
+
+                if (i == 55 && j == 56 && k == 57)
+                {
+                    putchar('\n');
+                    return 0;
+                }
+                else
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return 0;
+}
