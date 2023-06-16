b709509e0daa8c968f3f50155c58bbb360d0b251
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 10:35:30 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#include <unistd.h>
-#include <string.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    write(STDERR_FILENO, message, strlen(message));
-    return (1);
-}
-=======
 #include <unistd.h>
 #include <string.h>
 
@@ -30,4 +13,3 @@ char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 write(STDERR_FILENO, message, strlen(message));
 return (1);
 }
->>>>>>> 4f09570630609971993a27e03ddd5b1652d73181
