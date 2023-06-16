4f4e42efd09fdca34d5108c76cc428a54f80cf7e
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 12:35:45 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - alphabet in lowercase, followed by a new line.
+ * main - Entry point.
  *
- * Always return(0)
+ * This function prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 char a = 'a';
