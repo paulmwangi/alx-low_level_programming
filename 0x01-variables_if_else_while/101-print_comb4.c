98e168c5ba96dff41e0e998c634bd7bf4b85997d
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-16 14:42:29 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,35 +8,33 @@
  */
 int main(void)
 {
-    int i, j, k;
-
-    for (i = 48; i < 58; i++)
-    {
-        for (j = 48; j < 58; j++)
-        {
-            for (k = 48; k < 58; k++)
-            {
-                if (i == j || j == k || i == k)
-                {
-                    continue;
-                }
-                putchar(i);
-                putchar(j);
-                putchar(k);
-
-                if (i == 55 && j == 56 && k == 57)
-                {
-                    putchar('\n');
-                    return 0;
-                }
-                else
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+int i, j, k;
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58; j++)
+{
+for (k = 48; k < 58; k++)
+{
+if (i == j || j == k || i == k)
+{
+continue;
+}
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == 55 && j == 56 && k == 57)
+{
+putchar('\n');
+return 0;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return 0;
 }
