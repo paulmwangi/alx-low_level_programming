3f28cad2cc52a04717894c55cfb027c7ab50de4c
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-19 09:06:36 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,29 +1,23 @@
 #include <unistd.h>
-#include <string.h>
 #include <fcntl.h>
+#include <sys/syscall.h>
 
 /**
  * main - Entry point of the program
  *
  * This program prints the message "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
- * followed by a new line, to the standard error using the write function. It demonstrates how
+ * followed by a new line, to the standard error using the write system call. It demonstrates how
  * to write output directly to the standard error without using printf or puts functions.
  *
  * Return: Always 1 (indicating failure)
  */
 int main(void)
 {
-    int fd;
     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(message);
+    ssize_t len = sizeof(message) - 1;
 
-    fd = open("/dev/stderr", O_WRONLY);
-    if (fd == -1)
-        return (1);
+    if (syscall(SYS_write, STDERR_FILENO, message, len) != len)
+        return 1;
 
-    if (write(fd, message, len) != len)
-        return (1);
-
-    close(fd);
-    return (1);
+    return 1;
 }
