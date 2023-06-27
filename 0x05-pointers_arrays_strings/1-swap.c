56678ab2e54b5f04eca1a2ed822adf3b46b8da63
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-06-27 09:21:16 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.c"
+/**
+ * swap_int - swap integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Description: swaps the values of two integers
+ * Return: Always (0);
+ */
+
+
+void swap_int(int *a, int *b){
+
+  int temp;
+
+  
+  temp = *a;
+  *a = *b;
+  *b = temo;
+}
+
+
