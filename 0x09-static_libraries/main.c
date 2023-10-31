b817f9e0ad481e478dd8278395cce662b85e303e
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-10-31 22:14:42 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	_puts("\"its over anakin i have the higher ground\"\n\t- Obi Wan");
-	return (0);
+    int result;
+
+    // Example usage of functions from the static library
+    result = _isupper('A');
+    if (result)
+        printf("A is an uppercase character\n");
+    else
+        printf("A is not an uppercase character\n");
+
+    result = _isdigit('7');
+    if (result)
+        printf("7 is a digit\n");
+    else
+        printf("7 is not a digit\n");
+
+    char str[] = "Hello, World!";
+    int length = _strlen(str);
+    printf("Length of the string: %d\n", length);
+
+    // You can call other functions from the static library here
+
+    return (0);
 }
