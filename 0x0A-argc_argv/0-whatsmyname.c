ad3a7eb3d12246621abd7fc6eb8da7945a1fafa5
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-11-01 20:30:37 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    (void)argc; // Suppress unused parameter warning
+    (void)argc; /* Suppress unused parameter warning */
     printf("%s\n", argv[0]);
-    return 0;
+    return (0);
 }
