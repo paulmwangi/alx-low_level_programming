3ae1959b88266e24fc309976d875eee236af1640
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2023-11-01 16:59:06 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include "main.h"
 
 int main(void)
@@ -7,4 +5,4 @@ int main(void)
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
 }
->>>>>>> d127987ee1ad77089bab08049ac2594695ae4988
+
