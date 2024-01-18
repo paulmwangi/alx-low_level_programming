326c56a1cb90a9cb782f8eacb1c2728f0d3a6d50
paulmwangi <37366598+paulmwangi@users.noreply.github.com>
2024-01-18 23:14:18 +0300

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -c -fPIC *.c
-
-# Create the dynamic library
-gcc *.o -shared -o liball.so
+gcc -Wall -fPIC -c *.c
+gcc -shared -o liball.so *.o
